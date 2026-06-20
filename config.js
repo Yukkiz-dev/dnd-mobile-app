db2f072bb09d045851c2d4e505c1f9e152baa2fe
Yukkiz-dev <ianlim0503@gmail.com>
2026-06-20 18:20:47 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwY-DX41ktYSMe55hcocTijQObZzNovixQklCEUnS0_fD0VRlkNWigW_vR-M1jHdcmi9Q/exec"
+  //apiUrl: "https://script.google.com/macros/s/AKfycbwY-DX41ktYSMe55hcocTijQObZzNovixQklCEUnS0_fD0VRlkNWigW_vR-M1jHdcmi9Q/exec"
+  apiUrl: "https://script.google.com/macros/s/AKfycbyDU_lU7PgDLSNURerKGtLMNFSsrggQk7iTjcJO7_0eiHCRfmzusT0No5yyOnr2bB14wQ/exec"
 };
